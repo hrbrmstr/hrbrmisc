775b6ad8cab54d33906a0d4d9484874d0232abaf
Bob Rudis <bob@rudis.net>
2015-08-12 08:42:31 -0400

    bom

--- R/web.r
+++ R/web.r
@@ -1,8 +1,20 @@
-#' Tests whether raw httr response or character vector has a byte-order-mark
+#' Tests whether a raw httr response or character vector has a byte order mark (BOM)
+#'
+#' \href{https://en.wikipedia.org/wiki/Byte_order_mark}{What is a byte order mark?}
 #'
 #' @param resp \code{httr} response object or a character vector
-#' @return \code{TRUE} if response contains a byte-order-mark
+#' @param encoding can be one of \code{UTF-8}, \code{UTF-16} or \code{UTF-16BE}.
+#'        Although a BOM could be used with UTF-32 and other encodings, such
+#'        encodings are rarely used for data transmission and the three supported
+#'        encodings are the most likely ones folks in R will be working with from
+#'        web APIs.\cr\cr
+#'        This function defaults to looking for \code{UTF-8} BOM, but you can
+#'        override it.
+#' @return \code{TRUE} if response contains a BOM, \code{NA} if an unsupported encoding
+#'         was passed (along with a message)
 #' @export
+#' @references \href{http://www.unicode.org/faq/utf_bom.html}{UTF-8, UTF-16, UTF-32 & BOM}
+#' @author @@hrbrmstr
 #' @examples
 #' library(httr)
 #' url <- "http://www.claritin.com/webservice/allergyforecast.php?zip=33956"
@@ -11,12 +23,25 @@
 #'
 #' cfoo <- content(foo, as="text")
 #' has_bom(cfoo)
-has_bom <- function(resp) {
+#'
+#' has_bom(GET("http://google.com"))
+has_bom <- function(resp, encoding="UTF-8") {
   if (inherits(resp, "response")) {
-    foo$content[1] == as.raw(0xef) &
-      foo$content[2] == as.raw(0xbb) &
-      foo$content[3] == as.raw(0xbf)
+    F <- resp$content[1:4]
+    switch(encoding,
+           `UTF-8`=F[1]==as.raw(0xef) & F[2]==as.raw(0xbb) & F[3]==as.raw(0xbf),
+           `UTF-16`=F[1]==as.raw(0xff) & F[2]==as.raw(0xfe),
+           `UTF-16BE`=F[1]==as.raw(0xfe) & F[2]==as.raw(0xff),
+           { message("Unsupported encoding") ; return(NA) }
+    )
   } else {
-    grepl("^ï»¿", resp)
+    switch(encoding,
+           `UTF-8`=grepl("^ï»¿", resp[1]),
+           `UTF-16`=grepl("^þÿ", resp[1]),
+           `UTF-16BE`=grepl("^þÿ", resp[1]),
+           { message("Unsupported encoding") ; return(NA) }
+    )
   }
 }
+
+
