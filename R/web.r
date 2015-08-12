1d67d4007ddd0104ed71332e5f98a4cf72da6fa5
Bob Rudis <bob@rudis.net>
2015-08-12 13:10:12 -0400

    bom

--- R/web.r
+++ R/web.r
@@ -37,7 +37,7 @@ has_bom <- function(resp, encoding="UTF-8") {
     switch(encoding,
            `UTF-8`=grepl("^ï»¿", resp[1]),
            `UTF-16`=grepl("^þÿ", resp[1]),
-           `UTF-16BE`=grepl("^þÿ", resp[1]),
+           `UTF-16BE`=grepl("^ÿþ", resp[1]),
            { message("Unsupported encoding") ; return(NA) }
     )
   } else {
@@ -46,4 +46,50 @@ has_bom <- function(resp, encoding="UTF-8") {
   }
 }
 
+#' Remove byte order mark (BOM) from \code{httr::response} object or character vector
+#'
+#' Performs the equivalent of \code{content(resp, type="text")} but removes the
+#' BOM before doing so. If a BOM is not detected, the function will return an error.
+#'
+#' \href{https://en.wikipedia.org/wiki/Byte_order_mark}{What is a byte order mark?}
+#'
+#' @param resp \code{httr} response object or a character vector
+#' @return Converted content
+#' @export
+#' @references \href{http://www.unicode.org/faq/utf_bom.html}{UTF-8, UTF-16, UTF-32 & BOM}
+#' @author @@hrbrmstr
+#' @examples
+#' library(httr)
+#' foo <- GET("http://www.claritin.com/webservice/allergyforecast.php?zip=33956")
+#' sans_bom(foo)
+sans_bom <- function(resp) {
 
+  if (inherits(resp, "response")) {
+
+    F <- resp$content[1:4]
+    if (F[1]==as.raw(0xef) & F[2]==as.raw(0xbb) & F[3]==as.raw(0xbf)) {
+      iconv(readBin(resp$content[4:length(resp$content)], character()), from="UTF-8", to="UTF-8")
+    } else if (F[1]==as.raw(0xff) & F[2]==as.raw(0xfe)) {
+      iconv(readBin(resp$content[3:length(resp$content)], character()), from="UTF-16", to="UTF-8")
+    } else if (F[1]==as.raw(0xfe) & F[2]==as.raw(0xff)) {
+      iconv(readBin(resp$content[3:length(resp$content)], character()), from="UTF-16BE", to="UTF-8")
+    } else {
+      stop("Did not detect a BOM in the httr::response object content.", call.=FALSE)
+    }
+
+  } else if (inherits(resp, "character")) {
+
+    if (grepl("^ï»¿", resp[1])) {
+      iconv(readBin(sub("^ï»¿", "", resp), character()), from="UTF-8", to="UTF-8")
+    } else if (grepl("^þÿ", resp[1])) {
+      iconv(readBin(sub("^þÿ", "", resp), character()), from="UTF-16", to="UTF-8")
+    } else if (grepl("^ÿþ", resp[1])) {
+      iconv(readBin(sub("^ÿþ", "", resp), character()), from="UTF-16BE", to="UTF-8")
+    } else {
+      stop("Did not detect a BOM in the content.", call.=FALSE)
+    }
+
+  } else {
+    stop("Expected either an httr::response object or a character", call.=FALSE)
+  }
+}
