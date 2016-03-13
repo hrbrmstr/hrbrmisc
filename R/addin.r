58f02e717393683b78fa5d245a2e56024456cf2e
Bob Rudis <bob@rudis.net>
2016-03-13 10:30:23 -0400

    finished add-in as much as i'm going to :-)

--- R/addin.r
+++ R/addin.r
@@ -17,35 +17,54 @@ add_subtitle <- function() {
   ui <- miniPage(
     gadgetTitleBar("Add Subtitle"),
     miniContentPanel(
-      fillRow(height='75%', style="margin-bottom:10px",
+    fillRow(height="3%", width="100%",
+            tags$div("A glitch causes plot to disappear occassionally. Hit Enter in title text box to refresh.",
+                     style="color:#9ebcda; margin:auto; margin-top:5px; text-align:center; font-size:8pt")
+    ),
+    fillRow(height='57%', style="margin-bottom:10px",
+            miniContentPanel(
               plotOutput("curr_plot", width='100%', height='100%')
-      ),
-      fillRow(height="25%",
-              tags$div(style="display:table; width:99%; margin:auto",
-                       tags$textarea(id="subtitle", label="Subtitle",
-                                     rows=3, cols=40, "A boring subtitle",
-                                     style="width:inherit; padding=5px;"
-                       ),
-                       numericInput("wrap", "Wrap", "100", min = 10)
-              )
-      )
+            )
+    ),
+    fillRow(width="100%", height="10%",
+            tags$div(style="display:table; width:99%; margin:auto",
+                     tags$textarea(id="subtitle", label="Subtitle",
+                                   rows=2, cols=40, "A boring subtitle",
+                                   style="width:inherit; font-size:9pt; padding:5px; margin-bottom:300px"
+                     )
+            )
+    ),
+    fillRow(width="100%", height="9%",
+            textInput("fontfamily", "Font Family", value="Helvetica", width="130px"),
+            numericInput("wrap", "Wrap", value="100", min=10, width="100px"),
+            numericInput("fontsize", "Font size", value="10", min=1, width="100px")
+    ),
+    fillRow(width="100%", height="2%", tags$div()),
+    fillRow(width="100%", height="9%",
+            numericInput("hjust", "Horiz just", value="0", min=0, max=1, width="100px", step=0.5),
+            numericInput("vjust", "Vert just", value="0", min=0, max=1, width="100px", step=0.5),
+            numericInput("bottom_margin", "Bottom margin", value="5.5", min=0, width="100px")
+    )
     )
   )
 
   server <- function(input, output, session) {
 
     gg_reactive <- reactive({
-      wrap_len <- 100
-      if (length(input$wrap) > 0) {
-        wrap_len  <- input$wrap
-      }
-      subt <- paste0(strwrap(input$subtitle, wrap_len), sep="", collapse="\n")
-      ggplot_with_subtitle(gg_original, subt)
+
+      subt <- paste0(strwrap(input$subtitle, input$wrap), sep="", collapse="\n")
+
+      ggplot_with_subtitle(gg_original,
+                           label=subt,
+                           fontfamily=input$fontfamily,
+                           fontsize=input$fontsize,
+                           hjust=input$hjust,
+                           vjust=input$vjust,
+                           bottom_margin=input$bottom_margin)
+
     })
 
-    current_plot <- renderPlot({
-      gg_reactive()
-    })
+    current_plot <- renderPlot({ gg_reactive()  })
 
     output$curr_plot <- current_plot
 
