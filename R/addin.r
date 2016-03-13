11922740fe1626358633e282a7d85127e5dff868
Bob Rudis <bob@rudis.net>
2016-03-13 10:55:51 -0400

    gadget finished

--- R/addin.r
+++ R/addin.r
@@ -17,34 +17,35 @@ add_subtitle <- function() {
   ui <- miniPage(
     gadgetTitleBar("Add Subtitle"),
     miniContentPanel(
-    fillRow(height="3%", width="100%",
-            tags$div("A glitch causes plot to disappear occassionally. Hit Enter in title text box to refresh.",
-                     style="color:#9ebcda; margin:auto; margin-top:5px; text-align:center; font-size:8pt")
-    ),
-    fillRow(height='57%', style="margin-bottom:10px",
-            miniContentPanel(
-              plotOutput("curr_plot", width='100%', height='100%')
-            )
-    ),
-    fillRow(width="100%", height="10%",
-            tags$div(style="display:table; width:99%; margin:auto",
-                     tags$textarea(id="subtitle", label="Subtitle",
-                                   rows=2, cols=40, "A boring subtitle",
-                                   style="width:inherit; font-size:9pt; padding:5px; margin-bottom:300px"
-                     )
-            )
-    ),
-    fillRow(width="100%", height="9%",
-            textInput("fontfamily", "Font Family", value="Helvetica", width="130px"),
-            numericInput("wrap", "Wrap", value="100", min=10, width="100px"),
-            numericInput("fontsize", "Font size", value="10", min=1, width="100px")
-    ),
-    fillRow(width="100%", height="2%", tags$div()),
-    fillRow(width="100%", height="9%",
-            numericInput("hjust", "Horiz just", value="0", min=0, max=1, width="100px", step=0.5),
-            numericInput("vjust", "Vert just", value="0", min=0, max=1, width="100px", step=0.5),
-            numericInput("bottom_margin", "Bottom margin", value="5.5", min=0, width="100px")
-    )
+      fillRow(height="3%", width="100%",
+              tags$div("A glitch causes plot to disappear occassionally. Hit Enter in title text box to refresh.",
+                       style="color:#9ebcda; margin:auto; margin-top:5px; text-align:center; font-size:8pt")
+      ),
+      fillRow(height='57%', style="margin-bottom:10px",
+              miniContentPanel(
+                plotOutput("curr_plot", width='100%', height='100%')
+              )
+      ),
+      fillRow(width="100%", height="10%",
+              tags$div(style="display:table; width:99%; margin:auto",
+                       tags$textarea(id="subtitle", label="Subtitle",
+                                     rows=2, cols=40, "A boring subtitle",
+                                     style="width:inherit; font-size:9pt; padding:5px; margin-bottom:300px"
+                       )
+              )
+      ),
+      fillRow(width="100%", height="9%",
+              textInput("fontfamily", "Font Family", value="Helvetica", width="130px"),
+              numericInput("wrap", "Wrap", value="100", min=10, width="100px"),
+              numericInput("fontsize", "Font size", value="10", min=1, width="100px"),
+              numericInput("lineheight", "Line height", value="1", min=0, width="100px")
+      ),
+      fillRow(width="100%", height="2%", tags$div()),
+      fillRow(width="100%", height="9%",
+              numericInput("hjust", "Horiz just", value="0", min=0, max=1, width="100px", step=0.5),
+              numericInput("vjust", "Vert just", value="0", min=0, max=1, width="100px", step=0.5),
+              numericInput("bottom_margin", "Bottom margin", value="5.5", min=0, width="100px")
+      )
     )
   )
 
@@ -60,7 +61,8 @@ add_subtitle <- function() {
                            fontsize=input$fontsize,
                            hjust=input$hjust,
                            vjust=input$vjust,
-                           bottom_margin=input$bottom_margin)
+                           bottom_margin=input$bottom_margin,
+                           lineheight=input$lineheight)
 
     })
 
@@ -69,9 +71,27 @@ add_subtitle <- function() {
     output$curr_plot <- current_plot
 
     observeEvent(input$done, {
+
       rstudioapi::insertText(sprintf("subt <- '%s'\n", input$subtitle))
       rstudioapi::insertText(sprintf('subt <- paste0(strwrap(subt, %s), sep="", collapse="\\n")\n', input$wrap))
-      rstudioapi::insertText(sprintf("ggplot_with_subtitle(%s, subt)\n", rstudio_input_text))
+
+      command <- sprintf("ggplot_with_subtitle(%s, %s, %s, %s, %s, %s, %s, %s)\n",
+                         rstudio_input_text,
+                         "label=subst",
+                         sprintf("fontfamily='%s'", input$fontfamily),
+                         sprintf("fontsize='%s'", input$fontsize),
+                         sprintf("hjust='%s'", input$hjust),
+                         sprintf("vjust='%s'", input$vjust),
+                         sprintf("bottom_margin='%s'", input$bottom_margin),
+                         sprintf("lineheight='%s'", input$lineheight))
+
+      command <- formatR::tidy_source(text=command, output=FALSE,
+                                      width.cutoff=40)$text.tidy
+
+      rstudioapi::insertText(paste(command, collapse = "\n"))
+
+      invisible(stopApp())
+
     })
 
     observeEvent(input$cancel, {
