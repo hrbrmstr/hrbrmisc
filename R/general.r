2595a1ba591862e11864344b792b3917fc318806
Bob Rudis <bob@rudis.net>
2016-03-12 19:59:00 -0500

    ggplot

--- R/general.r
+++ R/general.r
@@ -1,23 +1,3 @@
-# these have been pilfered from various repos
-
-#' good for nulls
-#'
-#' good way of getting a value if \code{a} is \code{NULL}
-#'
-#' @rdname noathenb
-#' @export
-noathenb <- function(a, b) {
-  if (length(a) > 0) a else b
-}
-
-#' good for nulls
-#'
-#' good way of getting a value if \code{a} is \code{NULL}
-#'
-#' @rdname noathenb
-#' @export
-"%||%" <- noathenb
-
 #' Does the environment variable exist?
 #' @export
 has_env_var <- function(x) !identical(Sys.getenv(x), "")
