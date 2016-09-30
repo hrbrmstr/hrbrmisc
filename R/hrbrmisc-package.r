95ae39455cee23f572db36be6956df409ca5db1f
boB Rudis <bob@rud.is>
2016-09-30 10:16:18 -0400

    forgot to import %>%

--- R/hrbrmisc-package.r
+++ R/hrbrmisc-package.r
@@ -5,7 +5,7 @@
 #' @import ggplot2 gtable formatR
 #' @importFrom grid grid.draw grid.newpage gpar textGrob grobHeight
 #' @importFrom httr GET HEAD POST
-#' @importFrom dplyr count_ mutate
+#' @importFrom dplyr count_ mutate %>%
 #' @import broom
 #' @importFrom purrr flatten_df map map_df map_chr map_lgl map_int %||% safely
 #' @import stringi
