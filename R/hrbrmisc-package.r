57cb7352aec91d4ab32004195876d2b0dd895279
Bob Rudis <bob@rud.is>
2016-12-05 11:01:43 -0500

    quoted printable decoding

--- R/hrbrmisc-package.r
+++ R/hrbrmisc-package.r
@@ -1,4 +1,5 @@
 #' Personal R pkg
+#'
 #' @name hrbrmisc
 #' @docType package
 #' @author Bob Rudis (bob@@rud.is)
@@ -9,4 +10,19 @@
 #' @import broom
 #' @importFrom purrr flatten_df map map_df map_chr map_lgl map_int %||% safely
 #' @import stringi
+NULL
+
+
+#' hrbrmisc exported operators
+#'
+#' The following functions are imported and then re-exported
+#' from the hrbrmisc package to enable use of the magrittr
+#' pipe operator with no additional library calls
+#'
+#' @name hrbrmisc-exports
+NULL
+
+#' @name %>%
+#' @export
+#' @rdname hrbrmisc-exports
 NULL
