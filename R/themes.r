19a631948dad7bd012b8bdebaf26090e5167e728
Bob Rudis <bob@rud.is>
2017-02-09 11:47:24 -0500

    M+ font

--- R/themes.r
+++ R/themes.r
@@ -325,3 +325,103 @@ theme_hrbrmstr_kerkis <- function(grid = TRUE, axis = FALSE, ticks = FALSE,...) 
                  caption_size = 9,
                  grid=grid, axis=axis, ticks=ticks,...)
 }
+
+
+#' M+ font theme
+#'
+#' @export
+theme_hrbrmstr_mplus <- function(base_family="M+ 1c",
+                           base_size = 11,
+                           strip_text_family = "M+ 1c regular",
+                           strip_text_size = 12,
+                           plot_title_family="M+ 1c bold",
+                           plot_title_size = 18,
+                           plot_title_margin = 10,
+                           plot_margin = margin(30, 30, 30, 30),
+                           subtitle_family="M+ 1c medium",
+                           subtitle_size = 12,
+                           subtitle_margin = 15,
+                           caption_family="M+ 1c light",
+                           caption_size = 9,
+                           caption_margin = 10,
+                           axis_title_family = subtitle_family,
+                           axis_title_size = 9,
+                           axis_title_just = "rt",
+                           grid = TRUE,
+                           axis = FALSE,
+                           ticks = FALSE) {
+
+  ret <- theme_minimal(base_family=base_family, base_size=base_size)
+
+  ret <- ret + theme(legend.background=element_blank())
+  ret <- ret + theme(legend.key=element_blank())
+
+  if (inherits(grid, "character") | grid == TRUE) {
+
+    ret <- ret + theme(panel.grid=element_line(color="#2b2b2bdd", size=0.10))
+    ret <- ret + theme(panel.grid.major=element_line(color="#2b2b2b99", size=0.10))
+    ret <- ret + theme(panel.grid.minor=element_line(color="#2b2b2b99", size=0.05))
+
+    if (inherits(grid, "character")) {
+      if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x=element_blank())
+      if (regexpr("Y", grid)[1] < 0) ret <- ret + theme(panel.grid.major.y=element_blank())
+      if (regexpr("x", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.x=element_blank())
+      if (regexpr("y", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.y=element_blank())
+    }
+
+  } else {
+    ret <- ret + theme(panel.grid=element_blank())
+  }
+
+  if (inherits(axis, "character") | axis == TRUE) {
+    ret <- ret + theme(axis.line=element_line(color="#2b2b2b", size=0.15))
+    if (inherits(axis, "character")) {
+      axis <- tolower(axis)
+      if (regexpr("x", axis)[1] < 0) {
+        ret <- ret + theme(axis.line.x=element_blank())
+      } else {
+        ret <- ret + theme(axis.line.x=element_line(color="#2b2b2b", size=0.15))
+      }
+      if (regexpr("y", axis)[1] < 0) {
+        ret <- ret + theme(axis.line.y=element_blank())
+      } else {
+        ret <- ret + theme(axis.line.y=element_line(color="#2b2b2b", size=0.15))
+      }
+    } else {
+      ret <- ret + theme(axis.line.x=element_line(color="#2b2b2b", size=0.15))
+      ret <- ret + theme(axis.line.y=element_line(color="#2b2b2b", size=0.15))
+    }
+  } else {
+    ret <- ret + theme(axis.line=element_blank())
+  }
+
+  if (!ticks) {
+    ret <- ret + theme(axis.ticks = element_blank())
+    ret <- ret + theme(axis.ticks.x = element_blank())
+    ret <- ret + theme(axis.ticks.y = element_blank())
+  } else {
+    ret <- ret + theme(axis.ticks = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.x = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.y = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.length = grid::unit(5, "pt"))
+  }
+
+  xj <- switch(tolower(substr(axis_title_just, 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+  yj <- switch(tolower(substr(axis_title_just, 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+
+  ret <- ret + theme(axis.text.x=element_text(margin=margin(t=0)))
+  ret <- ret + theme(axis.text.y=element_text(margin=margin(r=0)))
+  ret <- ret + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
+  ret <- ret + theme(axis.title.x=element_text(hjust=xj, size=axis_title_size, family=axis_title_family))
+  ret <- ret + theme(axis.title.y=element_text(hjust=yj, size=axis_title_size, family=axis_title_family))
+  ret <- ret + theme(strip.text=element_text(hjust=0, size=strip_text_size, face="bold", family=strip_text_family))
+  ret <- ret + theme(panel.spacing.x=grid::unit(2, "lines"))
+  ret <- ret + theme(panel.spacing.y=grid::unit(2, "lines"))
+  ret <- ret + theme(plot.title=element_text(hjust=0, size=plot_title_size, margin=margin(b=plot_title_margin), face="bold", family=plot_title_family))
+  ret <- ret + theme(plot.subtitle=element_text(hjust=0, size=subtitle_size, margin=margin(b=subtitle_margin), family=subtitle_family))
+  ret <- ret + theme(plot.caption=element_text(hjust=1, size=caption_size, margin=margin(t=caption_margin), family=caption_family))
+  ret <- ret + theme(plot.margin=plot_margin)
+
+  ret
+
+}
